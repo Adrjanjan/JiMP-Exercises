c9b7f0121090f3aecf88a3a1ff02359fb29cfb50
Adrian Janakiewicz <adrjanjan@gmail.com>
2018-03-26 20:29:42 +0200

    CCounter all tests have passed.

--- lab3/ccounter/CCounter.cpp
+++ lab3/ccounter/CCounter.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "CCounter.h"
+
+
+namespace ccounter {
+
+    std::unique_ptr<Counter> Init() {
+        std::unique_ptr<Counter> counter_structure(new Counter);
+        counter_structure->objects = {};
+        counter_structure->instances_counter = {};
+        return std::move(counter_structure);
+    }
+
+    void Inc(std::string key, std::unique_ptr<Counter> *counter) {
+
+        (*counter)->objects.push_back(key);
+
+        auto it = (*counter)->instances_counter.find(key);
+        if (it != (*counter)->instances_counter.end()) {
+            ++(it->second);
+        } else {
+            (*counter)->instances_counter.emplace(key, 1);
+        }
+    }
+
+    int Counts(const std::unique_ptr<Counter> &counter, std::string key) {
+
+        auto it = counter->instances_counter.find(key);
+        if (it != counter->instances_counter.end()) {
+            return it->second;
+        } else {
+            return 0;
+        }
+
+    }
+
+    void SetCountsTo(std::string key, int value, std::unique_ptr<Counter> *counter) {
+
+        auto it = (*counter)->instances_counter.find(key);
+        if (it != (*counter)->instances_counter.end()) {
+            it->second = value;
+        } else {
+            (*counter)->instances_counter.emplace(key, 0);
+        }
+    }
+
+}
