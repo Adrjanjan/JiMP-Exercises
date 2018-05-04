c1e181af204f82f524d5b01b197e5abc41f4800c
Adrian Janakiewicz <adrjanjan@gmail.com>
2018-05-04 13:43:13 +0200

    Lab4 Template Engine Injection has passed.

--- lab4/netstemplateengine/SimpleTemplateEngine.cpp
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -13,25 +13,27 @@ nets::View::View(std::string template_str) {
 }
 
 std::string nets::View::Render(const std::unordered_map<std::string, std::string> &model) const {
-    std::regex capture_point_pattern{"(\\{\\{\\w+\\}\\})"};
-    std::string temp;
-    std::smatch capture_points;
-    std::string rendered = template_;
-    bool found;
+    std::regex capture_point_pattern{R"(\{\{([^{|}]+)\}\})"};
 
-    rendered = template_;
+    std::smatch capture_points, temp_capt_pts;
+    std::string rendered = template_;
+    std::string temp, final;
+
     while (std::regex_search(rendered, capture_points, capture_point_pattern)) {
-        found = false;
-        for (auto &n : model) {
-            temp = "{{" + n.first + "}}";
-            if (capture_points[0] == temp) {
-                found = true;
-                rendered.replace(capture_points.position(), capture_points.length(), n.second);
+
+        final += capture_points.prefix();
+        temp = capture_points.str();
+
+        if (std::regex_search(temp, temp_capt_pts, capture_point_pattern)) {
+            auto it = model.find(temp_capt_pts[1]);
+            if (it != model.end()) {
+                final += it->second;
             }
         }
-        if (!found) {
-            rendered.replace(capture_points.position(), capture_points[0].length(), "");
-        }
+        rendered = capture_points.suffix();
     }
-    return rendered;
+    final += rendered;
+
+    return final;
 }
+
