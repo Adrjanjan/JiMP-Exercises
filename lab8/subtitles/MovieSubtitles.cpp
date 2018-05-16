e224c1e3697c5e88420f36405629c99d074a63e7
Adrian Janakiewicz <adrjanjan@gmail.com>
2018-05-16 14:12:10 +0200

    Lab8 MicroDVD finished - all tests have passed.

--- lab8/subtitles/MovieSubtitles.cpp
+++ lab8/subtitles/MovieSubtitles.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "MovieSubtitles.h"
+
+void
+moviesubs::MicroDvdSubtitles::ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in,
+                                                  std::ostream *out) {
+
+    if (frame_per_second <= 0) throw SubtitlesException(0, "FPS is not positive: " + std::to_string(frame_per_second));
+
+    int true_fps = offset_in_micro_seconds * frame_per_second / 1000;
+    int line_count = 0;
+    int prev_first = 0;
+    std::pair<std::pair<int, int>, std::string> line;
+
+    std::smatch capture_points;
+    std::string subtitle_line, temp;
+
+    while (in->good()) {
+        std::getline(*in, subtitle_line);
+        if (subtitle_line.empty())
+            break;
+        ++line_count;
+        if (!std::regex_search(subtitle_line, capture_points, capture_point_pattern_)) {
+            throw InvalidSubtitleLineFormat(line_count, subtitle_line);
+        }
+
+        line = std::make_pair(std::make_pair(std::stoi(capture_points[1].str()), std::stoi(capture_points[2].str())),
+                              capture_points[3].str());
+
+        if (line.first.first > line.first.second) {
+            throw SubtitleEndBeforeStart(line_count, subtitle_line);
+        }
+
+        if (line.first.first + true_fps < 0) {
+            throw NegativeFrameAfterShift(line_count, subtitle_line);
+        }
+
+        if (prev_first > line.first.first) {
+            throw OutOfOrderFrames(line_count, subtitle_line);
+        }
+
+        prev_first = line.first.first;
+        (*out) << "{" << line.first.first + true_fps << "}{" <<
+               line.first.second + true_fps << "}" << line.second << std::endl;
+
+    }
+}
+
+moviesubs::SubtitlesException::SubtitlesException(int line_number, const std::string &message)
+        : std::invalid_argument("At line " + std::to_string(line_number) + ": " + message) {
+    wrong_line_number_ = line_number;
+}
+
+int moviesubs::SubtitlesException::LineAt() const {
+    return wrong_line_number_;
+}
+
+moviesubs::NegativeFrameAfterShift::NegativeFrameAfterShift(int line_number, const std::string &line)
+        : SubtitlesException(line_number, line) {}
+
+moviesubs::SubtitleEndBeforeStart::SubtitleEndBeforeStart(int line_number, const std::string &line)
+        : SubtitlesException(line_number, line) {}
+
+moviesubs::InvalidSubtitleLineFormat::InvalidSubtitleLineFormat(int line_number, const std::string &line)
+        : SubtitlesException(line_number, line) {}
+
+moviesubs::MissingTimeSpecification::MissingTimeSpecification(int line_number, const std::string &line)
+        : SubtitlesException(line_number, line) {}
+
+moviesubs::OutOfOrderFrames::OutOfOrderFrames(int line_number, const std::string &line)
+        : SubtitlesException(line_number, line) {}
--- lab8/subtitles/MovieSubtitles.h
+++ lab8/subtitles/MovieSubtitles.h
@@ -6,13 +6,79 @@
 #define JIMP_EXERCISES_MOVIESUBTITLES_H
 
 #include <sstream>
+#include <stdexcept>
+#include <regex>
 
 namespace moviesubs {
 
-    class MicroDvdSubtitles {
+    class MovieSubtitles {
     public:
-        ShiftAllSubtitlesBy(int, int, std::stringstream &, std::stringstream &);
+        virtual void
+        ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in, std::ostream *out) = 0;
+
+        ~MovieSubtitles() = default;
     };
+
+    class MicroDvdSubtitles : public MovieSubtitles {
+    public:
+        void ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in,
+                                 std::ostream *out) override;
+        // {INT_ON}{INT_OFF}SUBTITLE
+    private:
+        std::regex capture_point_pattern_{R"(\n?\{([0-9]+)\}\{([0-9]+)\}((\{.+\})?.+))"};
+    };
+
+    class SubtitlesException : public std::invalid_argument {
+    public:
+        SubtitlesException(int line_number, const std::string &message);
+
+        int LineAt() const;
+
+    private:
+        int wrong_line_number_;
+    };
+
+    class NegativeFrameAfterShift : public SubtitlesException {
+    public:
+        NegativeFrameAfterShift(int line_number, const std::string &line);
+    };
+
+    class SubtitleEndBeforeStart : public SubtitlesException {
+    public:
+        SubtitleEndBeforeStart(int line_number, const std::string &line);
+
+    };
+
+    class InvalidSubtitleLineFormat : public SubtitlesException {
+    public:
+        InvalidSubtitleLineFormat(int line_number, const std::string &line);
+    };
+
+    class SubRipSubtitles : public MovieSubtitles {
+    public:
+        void ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in,
+                                 std::ostream *out) override;
+        /* INDEX NEW_LINE
+         * TIME_IN → TIME_OFF NEW_LINE
+         * SUBTITLE1 NEW_LINE
+         * SUBTITLE2 NEW_LINE NEW_LINE
+         * gdzie INDEX - to numer klatki,
+         * NEW_LINE to znak nowej linii,
+         * TIME_IN i TIME_OFF to moment pojawienia się i zniknięcia klatki
+         * w formacie HH:MM:SS,mmm (godzina:minuta:sekundy,milisekundy).
+         * Napisy mogą być wielolinijkowe, nowa klatka pojawia się po pustej linii.*/
+    };
+
+    class MissingTimeSpecification : public SubtitlesException {
+    public:
+        MissingTimeSpecification(int line_number, const std::string &line);
+    };
+
+    class OutOfOrderFrames : public SubtitlesException {
+    public:
+        OutOfOrderFrames(int line_number, const std::string &line);
+    };
+
 }
 
 #endif //JIMP_EXERCISES_MOVIESUBTITLES_H
